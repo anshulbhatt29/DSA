a2db33b9b3d226af5d8ef53455b1143594b3c45a
anshulbhatt29 <anshulbhattddn@gmail.com>
2026-02-14 21:03:21 +0530

    Work Break

---
+++ Day1/src/WorkBreak.java
@@ -0,0 +1,30 @@
+class WordBreak {
+    public Boolean[] isValidWordBreak;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> words = new HashSet<>(wordDict);
+        int l=s.length();
+        isValidWordBreak = new Boolean[l];
+        return isWordBreak(s,0,words,l,isValidWordBreak);
+
+
+    }
+
+    public boolean isWordBreak(String s, int start, Set<String> words, int l,Boolean [] isValidWordBreak){
+        if(start==l)
+            return true;
+
+        if(isValidWordBreak[start]!=null)
+            return isValidWordBreak[start];
+
+        for(int end =start+1;end<=l;end++){
+            String sub = s.substring(start,end);
+            if(words.contains(sub) && isWordBreak(s,end,words,l,isValidWordBreak)){
+                return isValidWordBreak[start] = true;
+            }
+        }
+        return isValidWordBreak[start]=false;
+
+
+
+    }
+}
